db477dd25bb4d2da1af2d1e12a833c65a41af27d
Nehasingh1300 <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-22 23:36:44 +0530

    Create ArrowFunction.js

---
+++ ReactJS/Theory/ES6/Code/ArrowFunction.js
@@ -0,0 +1,10 @@
+// It makes thing done very easily
+
+var getRegValue = function(){
+  return 10;
+}
+console.log(getRegValue());
+
+const RegArrowValue = (m,bonus) => 10*m+bonus;
+console.log(RegArrowValue(5,6));
+console.log(typeof RegArrowValue);
