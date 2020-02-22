7ae3f8ca776a51ecec603e2269ce26fccbffe7bb
Nehasingh1300 <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-22 23:16:52 +0530

    Create greetPeople.js

---
+++ ReactJS/Theory/ES6/Code/greetPeople.js
@@ -0,0 +1,10 @@
+  
+function greetPerson(name){
+  if(name == Chandler){
+    var greet  = "Hello Chandler";
+  }else{
+    var greet = "Hi There!";
+  }
+  console.log(greet);
+}
+greetPerson("Chandler");
