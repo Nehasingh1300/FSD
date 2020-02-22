f01f78cf1107271991ad05ed1e9aaedc616ba942
Nehasingh1300 <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-22 23:31:54 +0530

    Create let_const.js

---
+++ ReactJS/Theory/ES6/Code/let_const.js
@@ -0,0 +1,10 @@
+//USE const when you dont have to change value of variable 
+// USE let when you need to change value of variable
+
+const PI = 3.14;
+const MAX_SIZE = 100;
+let a = 5;
+let b = 10;
+a= a+b;
+b= a-b;
+a = a-b;
