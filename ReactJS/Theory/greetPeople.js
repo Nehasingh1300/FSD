554df1472c319b8c5dac53451b5274af3cc29d87
Nehasingh1300 <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-22 23:16:28 +0530

    Delete greetPeople.js

--- ReactJS/Theory/greetPeople.js
+++
@@ -1,9 +0,0 @@
-function greetPerson(name){
-  if(name == Chandler){
-    var greet  = "Hello Chandler";
-  }else{
-    var greet = "Hi There!";
-  }
-  console.log(greet);
-}
-greetPerson("Chandler");
