b100e07d4c3db3bda111aec5f2c433472f683fc3
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-23 10:14:51 +0530

    Create ForOfLoop.ts

---
+++ ReactJS/Theory/ES6/Code/ForOfLoop.ts
@@ -0,0 +1,19 @@
+// for in will hav eto access every element with index
+
+let colors = ["Red","green","blue"];
+
+for( let index in colors){
+	console.log(colors[index]);
+}
+
+
+// for of can easily access all the elements of array
+for(let color of colors){
+	console.log(color);
+}
+
+let letters = "ABC";
+
+for(let letter of letters){
+	console.log(letter);
+}
