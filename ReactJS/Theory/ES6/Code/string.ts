b812a85d761dcf349a3751ed2879051f1cede27a
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-23 10:05:47 +0530

    Create string.ts

---
+++ ReactJS/Theory/ES6/Code/string.ts
@@ -0,0 +1,13 @@
+// working with strings
+
+let user = "Neha"
+
+let greet  = "Welcome "+user+ " to ES2015";
+let greet1 = `Welcome ${user} to ES2015
+				' HIII single cotes'
+				  "Hii with double cotes"
+				    this is 4th line here
+
+`;
+console.log(greet); 
+console.log(greet1); 
